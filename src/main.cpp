5ec2d4a193a3705099692fff44f6591b7bb0eff4
Riddhima06 <riddhima.rajput_cs.aiml24@gla.ac.in>
2026-02-21 15:34:04 +0530

    setup: initialize Serial communication at 9600 baud

--- src/main.cpp
+++ src/main.cpp
@@ -16,8 +16,7 @@ int fsrValue;
 
 void setup() {
 
-    // TODO 3:
-    // Initialize Serial communication (9600 baud rate)
+    Serial.begin(9600);
 
     // TODO 4:
     // Print system initialization message
