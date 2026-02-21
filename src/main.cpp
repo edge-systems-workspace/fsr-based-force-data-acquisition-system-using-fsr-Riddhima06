5fa52f9deb5eb5475495cfc73d3b671828d7c857
Riddhima06 <riddhima.rajput_cs.aiml24@gla.ac.in>
2026-02-21 15:32:57 +0530

    config: define FSR sensor analog pin as A0

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,15 @@
 /**
  * @file main.ino
  * @brief Embedded Force Measurement System using FSR
- * @author YOUR_NAME
- * @date YYYY-MM-DD
+ * @author Riddhima Rajput
+ * @date 2026-02-21
  *
  * @details
  * Reads analog force data from FSR sensor and
  * displays structured output via Serial Monitor.
  */
 
- // TODO 1:
- // Define FSR analog pin (Use A0)
+int fsrPin = A0;
 
  // TODO 2:
  // Create variable to store sensor reading
